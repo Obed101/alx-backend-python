81b416e24773af9991e9634459ffa144d96cb09e
Obed Amoako <obedamoako92@gmail.com>
2022-04-16 22:01:44 +0000

    imported typing for list of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Annotation of list of floats"""
+import typing
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: typing.list[float]) -> float:
     """Adds everything in input_list"""
     a: float = 0.00
     for i in input_list:
