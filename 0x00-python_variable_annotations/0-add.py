1aac1b9e4869a8c619266980d68a315ab483d5af
Obed Amoako <obedamoako92@gmail.com>
2022-04-16 20:40:21 +0000

    added a typed addition method

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Addition Module """
+
+def add(a: float, b: float):
+    """This function adds two arguments
+    and makes sure they are floats
+    """
+    return a + b
