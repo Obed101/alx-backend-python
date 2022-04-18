54d097e4dc102ead1eae495ca7a17ad35a5c00a4
Obed Amoako <obedamoako92@gmail.com>
2022-04-18 17:47:30 +0000

    validated file with mypy

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ This module uses async's concurrent coroutines """
 import asyncio
-import random
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, delay):
-    """ Uses a concurrent coroutine to call wait_random n times """
-    times = []
+async def wait_n(n: int, delay: int) -> List[float]:
+    """ Uses a concurrent coroutine to spawn wait_random n times """
+    times: List[float] = []
     for wait in asyncio.as_completed([wait_random(delay) for _ in range(n)]):
         one_time = await wait
         await times.append(float(one_time))
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module uses the time module to calculate elapse time """
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n, max_delay)
