ff0627f5a930f98cc9a8eaf7c03513803ecf676c
Obed Amoako <obedamoako92@gmail.com>
2022-04-18 16:10:13 +0000

    added a function that implements random and async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This is the first asyncio module """
+
+import asyncio, random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Waits for a random max_delay and returns it """
+    await asyncio.sleep(random.randint(0, wait_random))
+    return wait_random
