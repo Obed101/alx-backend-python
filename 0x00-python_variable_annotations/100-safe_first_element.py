b29cbe7a2c1ba5765939349ebcae1d14da66f4dc
Obed Amoako <obedamoako92@gmail.com>
2022-04-18 11:21:31 +0000

    debugged annotaions

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Fixing unknown annotations """
+from typing import Any, Sequence, Union
+
+
+# The types of the elements of the input are not known
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
