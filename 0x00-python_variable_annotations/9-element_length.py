1eff06f4896d7d28d0ca9c9e7755da0afb30e1e8
Obed Amoako <obedamoako92@gmail.com>
2022-04-18 11:09:51 +0000

    modified import

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ this module implements tuples of sequenses """
 
-from typing import Sequence, Tuple, List
+from typing import Sequence, Tuple, List, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ sequencially returns a tuple """
     return [(i, len(i)) for i in lst]
+
+print(element_length.__annotations__)
