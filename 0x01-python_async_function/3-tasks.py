b1951999a434beb3360d0fc3987472cf19c152d9
Obed Amoako <obedamoako92@gmail.com>
2022-04-19 18:33:27 +0000

    removed mypy typing

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,10 +3,10 @@
 
 import asyncio
 
-wait_random: float = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(delay: int) -> asyncio.Task:
     """ returns a <asyncio.Task> object from wait_random """
-    task: asyncio.Task = asyncio.create_task(wait_random(delay))
+    task = asyncio.create_task(wait_random(delay))
     return task
