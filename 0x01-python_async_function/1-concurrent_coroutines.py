dace5e78a3fa592fec4f333564918c04c0c41cfb
Obed Amoako <obedamoako92@gmail.com>
2022-04-19 19:28:41 +0000

    fixed mypy with timer

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ This module uses async's concurrent coroutines """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, delay: int) -> list:
+async def wait_n(n: int, delay: int) -> List[float]:
     """ Uses a concurrent coroutine to spawn wait_random n times """
     tasks = [asyncio.create_task(wait_random(delay)) for _ in range(n)]
     one_time = [await wait for wait in asyncio.as_completed(tasks)]
