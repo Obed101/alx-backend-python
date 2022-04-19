5160b36cbaa8c92b8adb756af99b5b3df5b82345
Obed Amoako <obedamoako92@gmail.com>
2022-04-19 17:40:09 +0000

    fixed mypy issues

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, delay: int) -> List[float]:
     """ Uses a concurrent coroutine to spawn wait_random n times """
-    tasks = [asyncio.create_task(wait_random(delay)) for _ in range(n)]
-    one_time = [await wait for wait in asyncio.as_completed(tasks)]
+    tasks: list = [asyncio.create_task(wait_random(delay)) for _ in range(n)]
+    one_time: List[float] = [await wait for wait in asyncio.as_completed(tasks)]
     return one_time
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,13 +2,14 @@
 """ This module uses the time module to calculate elapse time """
 import asyncio
 import time
+from typing import List
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n: List[float] = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """ calculates the runtime """
-    start = s = time.perf_counter()
+    start: float = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    elapsed = time.perf_counter() - start
+    elapsed: float = time.perf_counter() - start
     return float(elapsed / n)
