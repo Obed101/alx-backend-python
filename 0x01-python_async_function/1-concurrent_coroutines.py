9e1f190218f29ffaf69bcef2f3ce72facf164cce
Obed Amoako <obedamoako92@gmail.com>
2022-04-19 16:50:54 +0000

    fixed await issue

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,8 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, delay: int) -> List[float]:
     """ Uses a concurrent coroutine to spawn wait_random n times """
-    times: List[float] = []
-    for wait in asyncio.as_completed([wait_random(delay) for _ in range(n)]):
-        one_time = await wait
-        await times.append(float(one_time))
-    return times
+    tasks = [asyncio.create_task(wait_random(delay)) for _ in range(n)]
+    one_time = [await wait for wait in asyncio.as_completed(tasks)]
+    return one_time
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,4 +6,10 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n, max_delay)
+async def measure_time(n, max_delay):
+    """ calculates the runtime """
+    start = s = time.perf_counter()
+    await wait_n(n, max_delay)
+    elapsed = time.perf_counter() - start
+
+    return float(elapsed)
