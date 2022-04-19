b1a76aa3b7bdbce9037951067571cb656068c826
Obed Amoako <obedamoako92@gmail.com>
2022-04-19 17:03:00 +0000

    removed async from function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,10 +6,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n, max_delay):
+def measure_time(n, max_delay):
     """ calculates the runtime """
     start = s = time.perf_counter()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     elapsed = time.perf_counter() - start
-
-    return float(elapsed)
+    return float(elapsed / n)
