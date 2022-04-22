184894675a0e0356f3a1a1c4360bedcea2b47d4d
Obed Amoako <obedamoako92@gmail.com>
2022-04-22 11:48:59 +0000

    changed Iterator to Generator :new:

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import Iterator
+from typing import Generator
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """ This function uses async generator
     to generate numbers
     """
