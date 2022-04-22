ffb781f9bdf1dd5e45f53b0f76b1188e2324d133
Obed Amoako <obedamoako92@gmail.com>
2022-04-22 10:18:41 +0000

    changed output to floats

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """ This implements actual async comprehension """
 
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ Uses Async Generator to create Async Comprehension """
-    numbers = [i async for i in async_generator()]
+    numbers = [float(i) async for i in async_generator()]
     return numbers
