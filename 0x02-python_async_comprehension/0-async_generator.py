f076b47785874f81c9587474b4f6111c9a25ac57
Obed Amoako <obedamoako92@gmail.com>
2022-04-22 09:39:28 +0000

    finished async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,5 +2,14 @@
 """ This module uses async comprehension """
 
 import asyncio
+import random
+from typing import Iterator
 
 async def async_generator() -> Iterator[int]:
+    """ This function uses async comprehension
+    to generate numbers
+    """
+    number = random.randint(0, 10)
+    for _ in range(10):
+        asyncio.wait(1)
+        yield random.randint(0, 10)
