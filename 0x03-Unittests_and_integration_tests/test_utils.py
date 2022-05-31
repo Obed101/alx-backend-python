94351fdec280fec4c755d8e828d2d439cb3fbe4f
Obed Amoako <obedamoako92@gmail.com>
2022-05-31 20:53:32 +0000

    modified decoratoe

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",)),
         ({"a": {"b": 2}}, ("a", "b"))
-    ])
+        ])
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, output: Any) -> Any:
         """This function tests access_nested_map"""
@@ -22,7 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({}, ["a"]),
         ({"a": 1}, ["a", "b"])
-    ])
+        ])
     def test_access_nested_map_exception(self, nested_map: Mapping,
                                          path: Sequence, output: Any) -> Any:
         """Tests to see if access_nasted_map raises exception"""
